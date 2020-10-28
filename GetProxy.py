0a6d084b2b968fc31040c3042e56784ef84ea976
Kyriew007 <872344984@qq.com>
2020-10-28 23:35:01 +0800

    wuhu~

--- GetProxy.py
+++ GetProxy.py
@@ -4,9 +4,9 @@ import requests
 def GeneratPorxyNum(num):
     url = "http://dps.kdlapi.com/api/getdps/?orderid=910362950154248"
     url = url + "&num=%d&pt=1&format=json&sep=1" % num
-    # proxy = requests.get(url=url)
-    # proxy = proxy.json()
-    return url
+    proxy = requests.get(url=url)
+    proxy = proxy.json()['data']['proxy_list']
+    return proxy
 
 if __name__ == "__main__":
     num = int(input("please input proxy number:>>>"))
--- NikeGenerateAccount.py
+++ NikeGenerateAccount.py
@@ -12,13 +12,13 @@ def chooseSize(size):
 
 desired_capabilities = DesiredCapabilities.CHROME
 desired_capabilities["pageLoadStrategy"] = "none"
-# option = ChromeOptions()
-# ip = 'ip：port'
-# option.add_argument('--proxy-server=' + ip)
-# options=option
-driver = webdriver.Chrome()
+option = ChromeOptions()
+ip ='59.60.121.190:17515'
+option.add_argument('--proxy-server=' + ip)
+options=option
+driver = webdriver.Chrome(options=option)
 driver.maximize_window()
-# driver.get("https://www.nike.com/cn/")
+driver.get("https://www.nike.com/cn/")
 # time.sleep(5)
 # driver.find_element_by_xpath('//*[@id="AccountNavigationContainer"]/button').click()
 # time.sleep(3)
@@ -29,9 +29,9 @@ driver.maximize_window()
 # time.sleep(2)
 # driver.find_element_by_css_selector("[value='登录']").click()
 # time.sleep(5)
-driver.get('https://www.nike.com/cn/launch/t/lebron-18-sisterhood-cn')
+"""driver.get('https://www.nike.com/cn/launch/t/lebron-18-sisterhood-cn')
 time.sleep(5)
-chooseSize('41') 
+chooseSize('41')
 time.sleep(2)
 driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div/div[2]/div[2]/div/section[1]/div[2]/aside/div/div[2]/div/button').click()
-time.sleep(2)
+"""
